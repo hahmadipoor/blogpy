a7327dfc3df415266561a154a2959cce1763c5e2
hossein <ahmadipoor.h@gmail.com>
2022-04-06 19:54:16 +0430

    customized admin pannel

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.FileField(upload_to='files/user_avatar/', null=False, blank=False, validators=[validate_file_extension])
     description = models.CharField(max_length=512, null=False, blank=False)
-
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
 
@@ -31,6 +30,5 @@ class Article(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
     cover = models.FileField(upload_to='files/category_cover/', null=False, blank=False, validators=[validate_file_extension])
-
     def __str__(self):
         return self.title
